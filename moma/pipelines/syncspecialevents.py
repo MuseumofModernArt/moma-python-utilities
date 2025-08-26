95a8d121de3c4fd9d9b9d4c56f3a8a31680e0d92
John Halderman <john_halderman@moma.org>
2025-08-26 15:41:52 -0400

    fix: release data was removed from special_events

--- moma/pipelines/syncspecialevents.py
+++ moma/pipelines/syncspecialevents.py
@@ -9,7 +9,6 @@ class SpecialEvent(typing.NamedTuple):
         id: int
         slug: str
         salesforce_campaign: str
-        release_date: datetime.date
         turn_off_date: datetime.datetime
         created_at: datetime.datetime
         updated_at: datetime.datetime
@@ -26,7 +25,6 @@ class SpecialEvent(typing.NamedTuple):
             { 'name': 'id', 'type': 'INTEGER', 'mode': 'REQUIRED' },
             { 'name': 'slug', 'type': 'STRING', 'mode': 'NULLABLE' },
             { 'name': 'salesforce_campaign', 'type': 'STRING', 'mode': 'NULLABLE' },
-            { 'name': 'release_date', 'type': 'DATE', 'mode': 'NULLABLE' },
             { 'name': 'turn_off_date', 'type': 'TIMESTAMP', 'mode': 'NULLABLE' },
             { 'name': 'created_at', 'type': 'TIMESTAMP', 'mode': 'NULLABLE' },
             { 'name': 'updated_at', 'type': 'TIMESTAMP', 'mode': 'NULLABLE' },
@@ -45,7 +43,6 @@ class SpecialEvent(typing.NamedTuple):
                 id,
                 slug,
                 salesforce_campaign,
-                to_char(coalesce(release_date, '3000-01-01'::date), 'YYYY-MM-DD') as release_date,
                 to_char(coalesce(turn_off_date, '3000-01-01'::timestamp), 'YYYY-MM-DD HH24:MI:SS"."US') as turn_off_date,
                 to_char(created_at, 'YYYY-MM-DD HH24:MI:SS"."US') as created_at,
                 to_char(updated_at, 'YYYY-MM-DD HH24:MI:SS"."US') as updated_at,
@@ -59,8 +56,6 @@ class SpecialEvent(typing.NamedTuple):
 
     def to_dict(row):
         d = row._asdict()
-        if d['release_date'] == '3000-01-01':
-            d['release_date'] = None
         if d['turn_off_date'] == '3000-01-01 00:00:00.000000':
             d['turn_off_date'] = None
         if d['start_date'] == '3000-01-01':
