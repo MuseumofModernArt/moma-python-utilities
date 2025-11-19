4f6c3c28900c0dbe0d30c540b2b3c37c617c9729
Logan Cooper <logan_cooper@moma.org>
2025-11-19 14:16:12 -0800

    Add arguments to scripts

--- moma/pipelines/scripts/case-maker.py
+++ moma/pipelines/scripts/case-maker.py
@@ -1,16 +1,4 @@
-# change this out depending on which tables have been added
-TABLES = [
-    'events',
-    'price_lists',
-    'ticket_types',
-    'ticket_types_products',
-    'event_time_slots',
-    'event_tickets',
-    'price_lists_ticket_types',
-    'ticket_type_categories',
-    'events_ticket_type_categories',
-    'event_time_slots_products'
-]
+import sys
 
 def to_camel_case(snake_str: str) -> str:
     return "".join(x.capitalize() for x in snake_str.lower().split("_"))
@@ -19,8 +7,7 @@ def fill_case_statement(table_name: str) -> str:
     unspaced_table_name = table_name.replace('_', '')
     return f'''case "{table_name}":
     sync{unspaced_table_name} = importlib.import_module('moma.pipelines.sync{unspaced_table_name}')
-    make_runner(sync{unspaced_table_name}.{to_camel_case(table_name)})()'''
+    make_runner(sync{unspaced_table_name}.{to_camel_case(table_name)[:-1]})()'''
 
 if __name__ == '__main__':
-    for t in TABLES:
-        print(fill_case_statement(t))
+    print(fill_case_statement(sys.argv[1]))
