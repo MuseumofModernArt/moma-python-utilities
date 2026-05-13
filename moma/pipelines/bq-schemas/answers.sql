0bfcd38f896b793ec293f681818c9a8448331d1f
Logan Cooper <logan_cooper@moma.org>
2026-05-13 11:29:07 -0700

    Add answers table schemas

---
+++ moma/pipelines/bq-schemas/answers.sql
@@ -0,0 +1,55 @@
+CREATE OR REPLACE TABLE `moma-dw.moma_apps.answers` (
+    id INT64 NOT NULL,
+    question_id INT64,
+    line_item_id INT64,
+    user_id INT64,
+    event_id INT64,
+    type STRING NOT NULL,
+    question_text STRING NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    PRIMARY KEY (id) NOT ENFORCED
+)
+PARTITION BY TIMESTAMP_TRUNC(created_at, MONTH) OPTIONS (require_partition_filter = FALSE);
+
+CREATE OR REPLACE TABLE `moma-membership.moma_import.answers` (
+    id INT64 NOT NULL,
+    question_id INT64,
+    line_item_id INT64,
+    user_id INT64,
+    event_id INT64,
+    type STRING NOT NULL,
+    question_text STRING NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    PRIMARY KEY (id) NOT ENFORCED
+)
+PARTITION BY TIMESTAMP_TRUNC(created_at, MONTH) OPTIONS (require_partition_filter = FALSE);
+
+CREATE OR REPLACE TABLE `moma-dw.moma_apps_staging.answers` (
+    id INT64 NOT NULL,
+    question_id INT64,
+    line_item_id INT64,
+    user_id INT64,
+    event_id INT64,
+    type STRING NOT NULL,
+    question_text STRING NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    PRIMARY KEY (id) NOT ENFORCED
+)
+PARTITION BY TIMESTAMP_TRUNC(created_at, MONTH) OPTIONS (require_partition_filter = FALSE);
+
+CREATE OR REPLACE TABLE `moma-apps-staging.moma_import.answers` (
+    id INT64 NOT NULL,
+    question_id INT64,
+    line_item_id INT64,
+    user_id INT64,
+    event_id INT64,
+    type STRING NOT NULL,
+    question_text STRING NOT NULL,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP,
+    PRIMARY KEY (id) NOT ENFORCED
+)
+PARTITION BY TIMESTAMP_TRUNC(created_at, MONTH) OPTIONS (require_partition_filter = FALSE);
